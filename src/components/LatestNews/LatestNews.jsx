04a54b9369e07d576e6864550090d4e62200c527
MdRehanAli <mdrehanali21d@gmail.com>
2025-10-28 10:39:25 -0700

    data added

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -6,9 +6,9 @@ const LatestNews = () => {
         <div className='flex items-center gap-5 bg-base-200 p-4 '>
             <p className='text-base-100 bg-secondary px-6 py-3'>Latest</p>
             <Marquee pauseOnHover speed={60}>
-                <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, nostrum.</p>
-                <p className='font-bold  px-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, nostrum.</p>
-                <p className='font-bold pr-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, nostrum.</p>
+                <p className='font-bold'>Match Highlights: Argentina vs Brazil — as it happened! </p>
+                <p className='font-bold  px-8'>Match Highlights: Germany vs Spain — as it happened! </p>
+                <p className='font-bold pr-8'>Match Highlights: Germany vs Spain — as it happened! </p>
             </Marquee>
         </div>
     );
